8ca564b4b392dfa0d2b7dbf96f5607617224b2b0
ataerg <ataerg@github.com>
2025-04-17 13:09:41 +0200

    Added interceptor to inject auth token to http requests

---
+++ src/app/interceptors/token.interceptor.ts
@@ -0,0 +1,14 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { LoginService } from '../login.service';
+import { inject } from '@angular/core';
+
+
+const loginService = inject(LoginService);
+
+export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
+  return next(req.clone({
+    setHeaders: {
+      Authorization: `Bearer ${loginService.getAuthToken}`
+    }
+  }));
+};
